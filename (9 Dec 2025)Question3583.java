381ad6160de5842f974a00c72b7c747af65e5e5a
shyam-medh <baranwal07shyam@gmail.com>
2025-12-09 11:14:39 +0530

    Todays Leetcode Question

---
+++ (9 Dec 2025)Question3583.java
@@ -0,0 +1,66 @@
+// Count Special Triplets
+/*
+You are given an integer array nums.
+A special triplet is defined as a triplet of indices (i, j, k) such that:
+- 0 <= i < j < k < n, where n = nums.length
+- nums[i] == nums[j] * 2
+- nums[k] == nums[j] * 2
+Return the total number of special triplets in the array.
+Since the answer may be large, return it modulo 109 + 7.
+
+Example 1:
+Input: nums = [6,3,6]
+Output: 1
+Explanation:
+The only special triplet is (i, j, k) = (0, 1, 2), where:
+- nums[0] = 6, nums[1] = 3, nums[2] = 6
+- nums[0] = nums[1] * 2 = 3 * 2 = 6
+- nums[2] = nums[1] * 2 = 3 * 2 = 6
+
+Example 2:
+Input: nums = [0,1,0,0]
+Output: 1
+Explanation:
+The only special triplet is (i, j, k) = (0, 2, 3), where:
+- nums[0] = 0, nums[2] = 0, nums[3] = 0
+- nums[0] = nums[2] * 2 = 0 * 2 = 0
+- nums[3] = nums[2] * 2 = 0 * 2 = 0
+
+Example 3:
+Input: nums = [8,4,2,8,4]
+Output: 2
+Explanation:
+There are exactly two special triplets:
+- (i, j, k) = (0, 1, 3)
+  - nums[0] = 8, nums[1] = 4, nums[3] = 8
+  - nums[0] = nums[1] * 2 = 4 * 2 = 8
+  - nums[3] = nums[1] * 2 = 4 * 2 = 8
+- (i, j, k) = (1, 2, 4)
+  - nums[1] = 4, nums[2] = 2, nums[4] = 4
+  - nums[1] = nums[2] * 2 = 2 * 2 = 4
+  - nums[4] = nums[2] * 2 = 2 * 2 = 4
+ 
+Constraints:
+3 <= n == nums.length <= 105
+0 <= nums[i] <= 105
+
+*/
+
+//                                                        Solution: 👇
+
+class Solution {
+    public int specialTriplets(int[] nums) {
+        int n = 100001, mod = 1000000007;
+        long ans = 0;
+
+        int[] hash = new int[n], prev = new int[n];
+        for(int i=0; i<nums.length; i++) hash[nums[i]]++;
+
+        for(int i=1; i<nums.length-1; i++){
+            prev[nums[i-1]]++;
+            int j= nums[i], k=2*j;
+            if(k<n) ans += (long)prev[k] * (hash[k] - prev[k] - (j == 0 ? 1 : 0));
+        }
+        return (int)(ans % mod);
+    }
+}
