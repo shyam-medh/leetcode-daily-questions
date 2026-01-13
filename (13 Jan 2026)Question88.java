b44d28ddaf6f9ee9c75c36c7ac2c44cf82b28df1
shyam-medh <baranwal07shyam@gmail.com>
2026-01-13 22:57:45 +0530

    added the solutions of question

--- (13 Jan 2026)Question88.java
+++ (13 Jan 2026)Question88.java
@@ -40,3 +40,16 @@ nums2.length == n
 1 <= m + n <= 200
 -109 <= nums1[i], nums2[j] <= 109
 */
+
+//                                                      Solution: 👇
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int j=0, i=m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
