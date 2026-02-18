18d2828dd9ef43cba33529e88608fb74ed53ad66
shyam-medh <baranwal07shyam@gmail.com>
2026-02-18 13:44:21 +0530

    Todays LeetCode Question( Binary Tree Level Order Traversal )

---
+++ (18 Feb 2026)Question102.java
@@ -0,0 +1,66 @@
+// Binary Tree Level Order Traversal
+
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+
+Example 3:
+Input: root = []
+Output: []
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+*/
+
+//                                                      Solution: 👇
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> res = new ArrayList<>();
+            
+            while(n-- > 0){
+                TreeNode temp = q.peek();
+                q.poll();
+                res.add(temp.val);
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+            }
+            ans.add(res);
+        }
+        return ans;
+    }
+}
