e55de1b5b8c09c79f56e3e864cf42146e127ea6c
shyam-medh <baranwal07shyam@gmail.com>
2026-04-18 17:48:59 +0530

    Todays LeetCode Question( Mirror Distance of and Integer)

---
+++ (18 Apr 2026)Question.3783.java
@@ -0,0 +1,52 @@
+// Mirror Distance of an Integer
+
+/*
+You are given an integer n.
+Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+Return an integer denoting the mirror distance of n​​​​​​​.
+abs(x) denotes the absolute value of x.
+
+ 
+Example 1:
+Input: n = 25
+Output: 27
+Explanation:
+reverse(25) = 52.
+Thus, the answer is abs(25 - 52) = 27.
+
+
+Example 2:
+Input: n = 10
+Output: 9
+Explanation:
+reverse(10) = 01 which is 1.
+Thus, the answer is abs(10 - 1) = 9.
+
+
+Example 3:
+Input: n = 7
+Output: 0
+Explanation:
+Reverse(7) = 7.
+Thus, the answer is abs(7 - 7) = 0.
+ 
+
+Constraints:
+1 <= n <= 10^9
+*/
+
+//                                                     Solution: 👇
+
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n - reverse(n));
+    }
+    private int reverse(int x){
+        int rev = 0;
+        while(x > 0){
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+}
